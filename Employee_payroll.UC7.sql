3e42e84dca3778508c8d26b6edae29ca5fbb193c
KunigiMohan <kmohanr5@gmail.com>
2021-11-29 05:16:32 +0530

    Added : Using sum,avg,min,max and count function to do analysis by Male or Female.

---
+++ Employee_payroll.UC7.sql
@@ -0,0 +1,6 @@
+select sum(salary) from employee_payroll where Gender='f';
+select avg(salary) from employee_payroll where Gender='f';
+select min(salary) from employee_payroll where Gender='f';
+select max(salary) from employee_payroll where Gender='f';
+select count(salary) from employee_payroll where Gender='f';
+select count(Type) from employee_payroll where type = 'f';
